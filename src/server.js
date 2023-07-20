9f5ad892e583bb836b69e244b504e5a670883e74
m4zeboy <moises775azevedo@gmail.com>
2023-07-20 14:44:40 -0300

    Status codes

--- src/server.js
+++ src/server.js
@@ -15,9 +15,9 @@ const server = http.createServer((req, res) => {
       name: "John Doe",
       email: "johndoe@example.com"
     })
-    return res.end("Criação de usuário");
+    return res.writeHead(201).end();
   }
-  res.end("Hello ignite!");
+  res.writeHead(404).end();
 })
 
 server.listen(3333);
