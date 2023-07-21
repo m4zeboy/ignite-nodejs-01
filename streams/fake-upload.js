6ff6c83db9f2f5f193c79bb3c8ef4c3f7aae8d68
m4zeboy <moises775azevedo@gmail.com>
2023-07-21 09:44:33 -0300

    Aplicando streams no modulo http

---
+++ streams/fake-upload.js
@@ -0,0 +1,19 @@
+import { Readable } from 'node:stream';
+import axios from 'axios'
+
+class OneToHundredStream extends Readable {
+  index = 1;
+  _read() {
+    const i = this.index++;
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buffer = Buffer.from(String(i));
+        this.push(buffer);
+      }
+    }, 1000)
+  }
+}
+
+axios.post('http://localhost:3334', new OneToHundredStream());
