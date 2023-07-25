b6ae5f0b2038acc789730b0d14b225e8be96d6cb
m4zeboy <moises775azevedo@gmail.com>
2023-07-25 16:15:44 -0300

    Criando ID único universal

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { Database } from './database.js';
+import { randomUUID } from 'node:crypto';
+
 
 const database = new Database();
 
@@ -16,7 +18,7 @@ const server = http.createServer(async (req, res) => {
   if(method === "POST" && url === "/users") {
     const { name, email } = req.body;
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       email
     };
