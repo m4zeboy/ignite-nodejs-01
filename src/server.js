8081b1f9104fab85d7cc800424e49d234e52ca41
m4zeboy <moises775azevedo@gmail.com>
2023-07-28 14:31:50 -0300

    Capturando query parameters

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js';
+import { extractQueryParams } from './utils/extract-query-params.js';
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -13,9 +14,12 @@ const server = http.createServer(async (req, res) => {
 
   if(route) {
     const routeParams = req.url.match(route.path);
+    const { query, ...params } = routeParams.groups;
+    
+    req.params = params; 
 
-    req.params = {...routeParams.groups}
-
+    req.query = query ? extractQueryParams(query) : {};
+    
     return route.handler(req,res);
   }
 
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,8 +1,9 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g;
   const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)');
-  const pathRegex = new RegExp(`^${pathWithParams}`);
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
   // ^ = começa com
   // + = um ou mais
+  // ? = opcional
   return pathRegex;
 }
